1f98d023dfba57343bf50cc5dcfe256c0af9c9f8
Daniel <dcck888@Daniels-MacBook-Neo.local>
2026-08-06 22:24:22 +0800

    Deploy leaner expanded coverage build

--- _next/static/8uuQKFSTanU4Vy9CNUzph/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[brandHub]","\u002Fblog\u002F[slug]","\u002Fcar-problems\u002F[slug]","\u002Fissues\u002F[issueId]","\u002Fworkshops\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
